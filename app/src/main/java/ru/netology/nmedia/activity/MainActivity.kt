0cc56a0017a7f10470889be43326608cddc7f246
Sergei Khriapov <sergeikhriapov@gmail.com>
2024-11-28 12:27:58 +0300

    Fix: reset editing state before creating a new post

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -77,6 +77,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
         binding.save.setOnClickListener {
+            viewModel.cancelEditing()
             newPostLauncher.launch()
         }
     }
